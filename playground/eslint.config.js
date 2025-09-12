58fc2a678c7275b5bd2ef365bfdeea6369d94975
Sunny-117 <zhiqiangfu6@gmail.com>
2025-09-12 17:17:11 +0800

    feat: playground

---
+++ playground/eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import reactBoundary from 'eslint-plugin-react-boundary' // ✅ 引入插件对象
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-boundary": reactBoundary // ✅ 对象形式
+    },
+    rules: {
+      "react-boundary/require-boundary": "error"
+    },
+  },
+])
